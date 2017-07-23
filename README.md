62d25ac2b813da2aa5a0e1279f7093b8cbbca6f7
heena mahour <heena393@gmail.com>
2017-07-23 20:22:02 +0530

    changes

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # crossfilterplus
 Wrapper over crossfilter to manipulate data easily. It is developer friendly in nature.
+**Crossfilterplus** is a npm module for exploring large multivariate datasets in the browser in a config driven manner. Since it is built over crossfilter, it could supports extremely fast (<30ms) interaction with coordinated views, even with datasets containing a million or more records; we built it to power analytics for Square Register, allowing merchants to slice and dice their payment history fluidly.
 
+Want to learn more? [See the wiki.](https://github.com/fallacy321/crossfilterplus/wiki)
+Have a look at [home page](https://fallacy321.github.io/crossfilterplus/) for insights 
+
+## Status
+
+Crossfilterplus is **under active development, please raise issues/ enhancements and merge requests**. We welcome genuine bug-fixes and PRs. The main developer and author associated with crossfilterplus is Heena Mahour at the moment.
+
+A new [Crossfilterplus Organization](https://github.com/Crossfilterplus) has been created on Github.
+## Future plans
+
+- We need active contributors !!
+- Make nested grouping config driven
+- Config enhancements
+- Integrate it with [d3plus](https://github.com/alexandersimoes/d3plus)
--- index.js
+++ index.js
@@ -2,34 +2,76 @@ var crossfilter = require('crossfilter')
 
 exports.data = function(input) {
   exports.ndx = crossfilter(input)
+  
   return exports.ndx;
 }
 var internalConfig = {
   
 }
-exports.dimension = function(params) {
+var dimensionBuild = function(params) {
   //params could be a string or function
   if(typeof params == "string") {
-    return exports.ndx.dimension(function(d) {
-      return d[params]
+   internalConfig['dimension'] = exports.ndx.dimension(function(d) {
+      return d[params].toString()
     })
+    return internalConfig['dimension']
   }
   else if(typeof params == "function") {
-    return exports.ndx.dimension(params)
+    internalConfig['dimension'] = exports.ndx.dimension(params)
+    return internalConfig['dimension']
   }
   else if(typeof params == "object") {
-    return exports.ndx.dimension(function(d) {
+    internalConfig['dimension'] =  exports.ndx.dimension(function(d) {
       var temp = ''
+    //  console.log(params)
        for(var h = 0; h < params.length; h++ ) {
-	 temp += d[h]
+	 temp += d[params[h]].toString()
       
     }
     return temp;
     })
+    return internalConfig['dimension']
    
   }
   else {
     console.warn('Invalid dimension operation on crossfilterPlus')
   }
 }
+exports.dimension = dimensionBuild
+exports.group = function() {
+}
+
+exports.build = function(config) {
+  if(!config || (typeof config != 'object')) {
+    console.warn('Invalid configuration defined for build operation')
+  }
+  else {
+    if(!config.hasOwnProperty('data')) {
+      config['data'] = []
+    }
+    exports.ndx = crossfilter(config['data'])
+    if(!config.hasOwnProperty('dimension')) {
+      console.warn('Dimension is mandatory field in configuration')
+      return;
+    }
+    exports.dimension(config.dimension)
+    if(!config.hasOwnProperty('aggregate') || (config['aggregate'] == 'count')) {
+      internalConfig['group'] = internalConfig['dimension'].group()
+    }
+    else if(config['aggregate'] == 'sum') {
+      internalConfig['group'] = internalConfig['dimension'].group().reduceSum(function(d) {
+	return d[config.measure]? d[config.measure]: 0
+      })
+    }
+    else {
+      //TODO
+    }
+    return internalConfig
+    
+    
+    
+  }
+}
+
+
 
